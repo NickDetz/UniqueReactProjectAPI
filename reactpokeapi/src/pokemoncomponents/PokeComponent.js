461fc2dc6f9038c0c016c49da64d896a0af6457e
Deters <nicholasdeters47@live.com>
2021-02-08 15:14:08 -0800

    More Fun with hooks

--- reactpokeapi/src/pokemoncomponents/PokeComponent.js
+++ reactpokeapi/src/pokemoncomponents/PokeComponent.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react'
+import axios from 'axios';
+import {Container, Button} from 'react-bootstrap'
 
 const PokeComponent = () => {
-    const [pokimon, findPoke] = useState('');
+    const [pokimon, findPoke] = useState(0);
+
+    
 
     return (
         <div>
-            Hello From PokeComponent
+            <Container>
+            <p>Poki id looking for is {pokimon}</p>
+            <Button onClick={()=> findPoke(pokimon + 1)}>Click me</Button>
+            </Container>  
         </div>
     )
 }
