76c3e0568bc58c62e6f98507c0422d6de7310405
Deters <nicholasdeters47@live.com>
2021-02-09 08:33:50 -0800

    more progress with th useState commands

--- reactpokeapi/src/pokemoncomponents/PokeComponent.js
+++ reactpokeapi/src/pokemoncomponents/PokeComponent.js
@@ -1,14 +1,27 @@
 import React, {useState} from 'react'
 import axios from 'axios';
-import {Container, Button} from 'react-bootstrap'
+import {Container, Button, FormControl, InputGroup} from 'react-bootstrap'
 
 const PokeComponent = () => {
     const [pokimon, findPoke] = useState(0);
+    const [pokiname, pokeName] = useState('');
 
-    
+    const Consoling = (pokiname) => {
+        console.log(pokiname)
+    }
 
     return (
         <div>
+             <InputGroup className="container-sm" >
+                <InputGroup.Prepend>
+                     <InputGroup.Text id="inputGroup-sizing-default">Name</InputGroup.Text>
+                        </InputGroup.Prepend>
+                         <FormControl type='text' placeholder="Pokimon Name" className='mr-sm-2'
+                            onChange = {e => pokeName(e.target.value)}
+
+                         />
+                        </InputGroup>
+                        <Button onClick = {()=> Consoling(pokiname)}> Bleg Button</Button>
             <Container>
             <p>Poki id looking for is {pokimon}</p>
             <Button onClick={()=> findPoke(pokimon + 1)}>Click me</Button>
