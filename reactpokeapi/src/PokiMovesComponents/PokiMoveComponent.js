07d4a1ef31f598bb5f2b9db5c1af6e7ebf9b1570
Deters <nicholasdeters47@live.com>
2021-02-10 10:31:44 -0800

    added some custom assets for type image moves in poki moves

--- reactpokeapi/src/PokiMovesComponents/PokiMoveComponent.js
+++ reactpokeapi/src/PokiMovesComponents/PokiMoveComponent.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
-import {Card, Container, Row, Col} from 'react-bootstrap'
+import {Card, Container, Row, Col, Button} from 'react-bootstrap'
 import PokiInterface from './PokiInterface';
+import PokiMovePic from './PokiMovePic';
+
+
 
 const PokiMoveComponent = () => {
     const [pokiMoveId1, pokeMoveId1] = useState(1)
@@ -17,10 +20,26 @@ const PokiMoveComponent = () => {
         <div>
             <Container>
                <Row>
-                <Col lg={3}>column 1 </Col>
-                <Col lg={3}>column 2 </Col>
-                <Col lg={3}>column 3 </Col>
-                <Col lg={3}>column 4 </Col>
+                <Col lg={3}><Card style={{ width: '18rem' }}>
+                     <PokiMovePic />
+                        <Card.Body>
+                         <Card.Title>Card Title</Card.Title>
+                            <Card.Text>
+                             Some quick example text to build on the card title and make up the bulk of
+                             the card's content.
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                            </Card.Body>
+                </Card></Col>
+                <Col lg={3}>
+                <PokiMovePic />
+                    column 2 </Col>
+                <Col lg={3}>
+                <PokiMovePic />
+                column 3 </Col>
+                <Col lg={3}>
+                <PokiMovePic />
+                column 4 </Col>
                 </Row>
             </Container>
 
---
+++ reactpokeapi/src/PokiMovesComponents/PokiMovePic.js
@@ -0,0 +1,29 @@
+import bug from './PokemonassetsPokiApi/bugtype.png';
+import dark from './PokemonassetsPokiApi/darktype.png';
+import dragon from './PokemonassetsPokiApi/dragontype.png';
+import electric from './PokemonassetsPokiApi/electrictype.png';
+import fairy from './PokemonassetsPokiApi/fairytype.png';
+import fighting from './PokemonassetsPokiApi/fightingtype.png';
+import fire from './PokemonassetsPokiApi/firetype.png';
+import flying from './PokemonassetsPokiApi/flyingtype.png';
+import ghost from './PokemonassetsPokiApi/ghosttype.png';
+import grass from './PokemonassetsPokiApi/grasstype.png';
+import ground from './PokemonassetsPokiApi/groundtype.png';
+import ice from './PokemonassetsPokiApi/icetype.png';
+import normal from './PokemonassetsPokiApi/normaltype.png';
+import poisen from './PokemonassetsPokiApi/poisentype.png';
+import pyschic from './PokemonassetsPokiApi/pyschictype.png';
+import rock from './PokemonassetsPokiApi/rocktype.png';
+import steel from './PokemonassetsPokiApi/steeltype.png';
+import water from './PokemonassetsPokiApi/watertype.png';
+import React from 'react'
+import {Card} from 'react-bootstrap'
+
+const  PokiMovePic = () => {
+    
+    return (
+        <Card.Img src={bug}></Card.Img>
+    )
+}
+
+export default PokiMovePic;
