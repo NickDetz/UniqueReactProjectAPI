ea793e9226a42bf78d79962c3b854be5c34c0ffe
Deters <nicholasdeters47@live.com>
2021-02-19 12:55:32 -0800

    Initialiaze Context learning how to use it

---
+++ reactpokeapi/src/context/PokemonContext/PokemonState.js
@@ -0,0 +1,19 @@
+import React, {useReducer} from 'react';
+import axios from 'axios';
+import pokemonContext from './PokemonContext';
+import PokeReducer from './PokeReducer';
+import {
+    GETPOKIMON,
+    CLEARPOKIMON,
+    GETPOKIMOVE1,
+    GETPOKIMOVE2,
+    GETPOKIMOVE3,
+    GETPOKIMOVE4
+} from '../types'
+
+const PokemonState = props => {
+    const initialState = {
+        id : 'id'
+
+    }
+}
