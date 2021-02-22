2bd184a710ac87b09b5c6799a73fe8b9a3f2ecd5
Deters <nicholasdeters47@live.com>
2021-02-22 08:34:03 -0800

    going to work on the poke battle (called pokibuilder right now will change it later

--- reactpokeapi/src/PokeBuilders/PokiBuilder.js
+++ reactpokeapi/src/PokeBuilders/PokiBuilder.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import {Container, Col, Row} from 'react-bootstrap';
 
 
 
  const PokiBuilder = () => {
+
+    const [pokeRed, getPokiRed] = useState();
+    const [pokeBlue, getPokiBlue] = useState();
+
+    useEffect(() => {
+            axios.get(`https://pokeapi.co/api/v2/pokemon/${1}/`).then(res => {
+                console.log(res.data)
+                getPokiRed(res.data)
+    })
+    },[])
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${2}/`).then(res => {
+            console.log(res.data)
+            getPokiBlue(res.data)
+})
+},[])
+
+
+
+
     return (
         
         <Container>
